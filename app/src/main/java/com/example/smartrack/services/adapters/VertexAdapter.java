b02d8a968874d6c0f27f7a1e4b9bc9bbda96746b
Bar Livni <barlivni@gmail.com>
2020-12-13 13:18:12 +0200

    add item activity+recycle view

--- app/src/main/java/com/example/smartrack/services/adapters/VertexAdapter.java
+++ app/src/main/java/com/example/smartrack/services/adapters/VertexAdapter.java
@@ -1,17 +1,21 @@
 package com.example.smartrack.services.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.smartrack.R;
 import com.example.smartrack.models.Vertex;
+import com.example.smartrack.ui.AddItem;
+
 
 import java.util.List;
 
@@ -37,12 +41,16 @@ public class VertexAdapter extends RecyclerView.Adapter<VertexAdapter.ViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull VertexAdapter.ViewHolder holder, int position) {
+        //bind the textView with data recieve
         Vertex vertexRow=verticesList.get(position);
+
+        int vId=vertexRow.id;
 
         holder.textViewCity.setText(vertexRow.city);
         holder.textViewAddress.setText(vertexRow.address);
         holder.textViewNumberItem.setText(vertexRow.itemsInVertex.size());
         holder.textViewZipcode.setText(vertexRow.zipcode);
+        //holder.imageViewAdd.set // holder.onClick(position)
     }
 
     @Override
@@ -62,7 +70,23 @@ public class VertexAdapter extends RecyclerView.Adapter<VertexAdapter.ViewHolder
             textViewAddress=(TextView) itemView.findViewById(R.id.textViewAddress);
             textViewNumberItem=(TextView) itemView.findViewById(R.id.textViewNumberItem);
             textViewZipcode=(TextView) itemView.findViewById(R.id.textViewZipcode);
-            imageViewAdd=itemView.findViewById(R.id.imageViewAdd);
+            imageViewAdd=(ImageView) itemView.findViewById(R.id.imageViewAdd);
+        }
+
+        //set on click listener
+        public void onClick(int position){
+            imageViewAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //sending Data from recycleView to addItem
+                    //Toast.makeText(context,position+"is clicked",Toast.LENGTH_LONG).show();
+                    //Vertex vertexRow=verticesList.get(position);
+                    //int vId=vertexRow.id;
+                    Intent intent = new Intent(v.getContext(),AddItem.class);
+                    intent.putExtra("vertexId",verticesList.get(position).id);
+                    v.getContext().startActivity(intent);
+                }
+            });
         }
     }
 
--- app/src/main/java/com/example/smartrack/ui/AddVertex.java
+++ app/src/main/java/com/example/smartrack/ui/AddVertex.java
@@ -1,22 +1,39 @@
 package com.example.smartrack.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.widget.Adapter;
+import android.widget.Toast;
 
 import com.example.smartrack.R;
 import com.example.smartrack.models.Globals;
 import com.example.smartrack.models.Graph;
+import com.example.smartrack.models.Vertex;
+import com.example.smartrack.services.adapters.VertexAdapter;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddVertex extends AppCompatActivity {
 
-    RecyclerView verticesList;
-    Adapter adapter;
+    RecyclerView recyclerView;
+    VertexAdapter vAdapter;   //RecyclerView.Adapter adapter;||Adapter adapter;
+
+    List<Vertex> verticesList;
+    DatabaseReference vertexRef;
 
 
     @Override
@@ -24,19 +41,30 @@ public class AddVertex extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_vertex);
 
-        verticesList=findViewById(R.id.recyclerViewVertices);
+        recyclerView=findViewById(R.id.recyclerViewVertex);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // set Globals
-        ((Globals) this.getApplication()).setVertexCounter(0);
-        ((Globals) this.getApplication()).setEdgeCounter(0);
-        ((Globals) this.getApplication()).setItemCounter(0);
+        verticesList=new ArrayList<>();
 
-        // get Globals
-        int vertexCounter = ((Globals) this.getApplication()).getVertexCounter();
-        int edgeCounter = ((Globals) this.getApplication()).getEdgeCounter();
-        int itemCounter = ((Globals) this.getApplication()).getItemCounter();
-
-
-
+        vertexRef= FirebaseDatabase.getInstance().getReference().child("Vertex");
+        vertexRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren())
+                {
+                    Vertex v=dataSnapshot.getValue(Vertex.class);
+                    verticesList.add(v);
+                }
+                vAdapter=new VertexAdapter(verticesList,AddVertex.this);
+                recyclerView.setAdapter(vAdapter);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(AddVertex.this,"Oops... Could not read from Db",Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
 }
